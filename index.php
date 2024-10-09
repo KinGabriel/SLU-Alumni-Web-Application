5073d99ac4417f58a2bc55cc6f2f53308da22817
Joaquin Gabriel Caparas <2235008@slu.edu.ph>
2024-10-10 02:07:09 +0800

    Set up the project structure and started the header

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<div class="header_seperator"></div>
+<header>
+    <h1>1</h1>
+        <div class="header_middle">
+            <a href="#">Home</a>
+            <a href="#">About Us</a>
+            <a href="#">Alumni Stories</a>
+            <a href="#">Career Opportunities</a>
+        </div>
+        <button class="btn-login"type="button">Log in</button>
+    </div>
+</header>
+</body>
+</html>
