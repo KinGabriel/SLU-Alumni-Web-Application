597d7aff9a20ea13036eaf63432f1beb13dc0540
Joaquin Gabriel Caparas <2235008@slu.edu.ph>
2024-10-13 01:32:58 +0800

    Worked on the log in form which is almost done

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <a href="#">Alumni Stories</a>
         <a href="#">Career Opportunities</a>
     </nav>
-    <button class="btn-login">Log In</button>
+    <button class="btn-login" onclick="location.href='LogInAndRegister.php'">Log In</button>
 </header>
 
 <div class="hero">
@@ -49,22 +49,7 @@
         </div>
     </div>
 </div>
-
-<script>
-let slideIndex = 0;
-automateSlides();
-
-function automateSlides() {
-    let i;
-    let slides = document.getElementsByClassName("slides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex - 1].style.display = "block";  
-    setTimeout(showSlides, 7000); 
-}
+<script src="assets/js/event-handler.js">
 </script>
 </body>
 </html>
