7e06d2ce2cb8941fc39c0887f430d2c711bb062d
nchljhy <njhoyescano@gmail.com>
2024-10-17 22:26:53 +0800

    feat: Worked on the hovering of sidenav bar and user request table

---
+++ assets/js/userRequest.js
@@ -0,0 +1,10 @@
+const sidenav = document.querySelector('.sidebar-container'); 
+
+// Add hover event to sidenav
+sidenav.addEventListener('mouseenter', () => {
+  mainContent.style.left = '5px'; // Move content to the right
+});
+
+sidenav.addEventListener('mouseleave', () => {
+  mainContent.style.left = '100px'; // Move content back to original position
+});
