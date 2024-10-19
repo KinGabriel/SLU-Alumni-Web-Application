512ffe958b89a89b59b795147ca8b135ad28da96
nchljhy <njhoyescano@gmail.com>
2024-10-20 04:12:18 +0800

    changed file extension

--- view/Adduser.php
+++ view/Adduser.php
@@ -28,13 +28,13 @@
                     </a>
              </li>
                 <li>
-                    <a href="/view/userRequest.html">
+                    <a href="/view/userRequest.php">
                     <img src="../assets/images/userRequest.png" alt="User Request" class="sidebar-icon">
                     <span class="menu-item-text">Account Requests</span>
                 </a>
                 </li>
                 <li>
-                    <a href="/view/account.html">
+                    <a href="/view/account.php">
                         <img src="../assets/images/userAccounts.png" alt="User Accounts" class="sidebar-icon">
                         <span class="menu-item-text">Accounts</span>
                     </a>
