02f4a7f0f9bb8c0c46a67131ec813418edd29a90
MINETTE VICTORIA MANDAC <2235117@slu.edu.ph>
2024-10-20 02:05:17 +0800

    physical database update

--- database/slu_alumina.sql
+++ database/slu_alumina.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `slu_alumina` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `slu_alumina` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `slu_alumina`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
@@ -28,13 +28,13 @@ CREATE TABLE `alumni` (
   `user_id` int NOT NULL,
   `school_id` int NOT NULL,
   `gradyear` int NOT NULL,
-  `program` varchar(80) NOT NULL,
-  `courses` varchar(80) NOT NULL,
+  `program` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `courses` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `is_employed` tinyint NOT NULL,
   PRIMARY KEY (`user_id`),
   KEY `program_idx` (`program`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -55,15 +55,17 @@ DROP TABLE IF EXISTS `applicants`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `applicants` (
   `app_id` int NOT NULL AUTO_INCREMENT,
-  `school_id` int NOT NULL,
-  `program` varchar(80) NOT NULL,
+  `lname` varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
+  `fname` varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
+  `pword` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `program` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `gradyear` int NOT NULL,
-  `id_proof` blob NOT NULL,
+  `school_id` int NOT NULL,
+  `school_id_pic` blob NOT NULL,
   `is_verified` tinyint NOT NULL,
-  PRIMARY KEY (`app_id`),
-  KEY `school_id_idx` (`school_id`),
-  CONSTRAINT `school_id` FOREIGN KEY (`school_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`app_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -85,14 +87,14 @@ DROP TABLE IF EXISTS `changelogs`;
 CREATE TABLE `changelogs` (
   `logid` int NOT NULL,
   `user_id` int NOT NULL,
-  `logact` text NOT NULL,
+  `logact` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `target_id` int NOT NULL,
-  `target_type` enum('post','user') NOT NULL,
+  `target_type` enum('post','user') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `timedate` datetime NOT NULL,
   PRIMARY KEY (`logid`),
   KEY `user_id_idx` (`user_id`),
   CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,7 +122,7 @@ CREATE TABLE `comments` (
   KEY `user_id_fkey_idx` (`user_id`),
   CONSTRAINT `post_id_fk` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -141,13 +143,13 @@ DROP TABLE IF EXISTS `event`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `event` (
   `event_id` int NOT NULL,
-  `event_title` varchar(80) NOT NULL,
-  `event_type` varchar(80) NOT NULL,
+  `event_title` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `event_type` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `event_time` datetime NOT NULL,
-  `event_location` varchar(80) NOT NULL,
+  `event_location` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`event_id`),
   CONSTRAINT `event_id_fk` FOREIGN KEY (`event_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -174,7 +176,7 @@ CREATE TABLE `follows` (
   KEY `followed_id_fk_idx` (`followed_id`),
   CONSTRAINT `followed_id_fk` FOREIGN KEY (`followed_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `follower_id_fk` FOREIGN KEY (`follower_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -202,7 +204,7 @@ CREATE TABLE `likes` (
   KEY `user_id_fkey2_idx` (`user_id`),
   CONSTRAINT `post_id_fkey` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `user_id_fkey2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -223,12 +225,12 @@ DROP TABLE IF EXISTS `opportunity`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `opportunity` (
   `opportunity_id` int NOT NULL,
-  `company_name` varchar(60) NOT NULL,
-  `program_preference` varchar(80) NOT NULL,
+  `company_name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `program_preference` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`opportunity_id`),
   KEY `program_preference_fk_idx` (`program_preference`) /*!80000 INVISIBLE */,
   CONSTRAINT `program_preference_fk` FOREIGN KEY (`program_preference`) REFERENCES `alumni` (`program`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -250,16 +252,16 @@ DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
   `post_id` int NOT NULL,
   `poster_id` int NOT NULL,
-  `description` text NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `banner` blob,
   `datetime` datetime NOT NULL,
   `is_deleted` tinyint NOT NULL,
-  `access_type` enum('public','private') NOT NULL,
+  `access_type` enum('public','private') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `post_type` enum('event','opportunities','normal') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`post_id`),
   KEY `poster_id_idx` (`poster_id`),
   CONSTRAINT `poster_id` FOREIGN KEY (`poster_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -280,16 +282,15 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `user_id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(70) NOT NULL,
-  `pword` varchar(45) NOT NULL,
-  `fname` varchar(70) NOT NULL,
-  `mname` varchar(70) DEFAULT NULL,
-  `lname` varchar(70) NOT NULL,
-  `contactno` varchar(45) NOT NULL,
+  `email` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `pword` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `fname` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `lname` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `contactno` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `pfp` blob,
   `user_type` enum('alumni','admin','manager') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -310,4 +311,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-19  1:45:08
+-- Dump completed on 2024-10-20  2:04:52
