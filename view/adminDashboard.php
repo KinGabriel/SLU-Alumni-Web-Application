a7a2fbf5f1e173bc5d0a2a089c0defbcdd9d185f
nchljhy <njhoyescano@gmail.com>
2024-11-02 23:24:45 +0800

    fixed the header and side nav bar

--- view/adminDashboard.php
+++ view/adminDashboard.php
@@ -12,7 +12,7 @@ unset($_SESSION['confirmationMessage'], $_SESSION['formData']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SLU Alumina</title>
     <link rel="stylesheet" href="../assets/css/adminDashboard.css">
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">  -->
 </head>
