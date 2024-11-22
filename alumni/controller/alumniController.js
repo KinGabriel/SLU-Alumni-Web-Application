ddf8cb6aa88ea003233b3bda196b020994428524
KinGabriel <joaquingabriel803@gmail.com>
2024-11-22 21:33:31 +0800

    log in redirection for node js is now working

--- alumni/controller/alumniController.js
+++ alumni/controller/alumniController.js
@@ -1,7 +1,12 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import dbConnection from '../../database/connection.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const getAlumni = (req, res) => {
-    const userId = req.cookies.user_id;  
+    const userId = req.cookies.user_id;
     if (!userId) {
         return res.status(400).send('User ID is missing in cookies!');
     }
@@ -9,14 +14,14 @@ export const getAlumni = (req, res) => {
     const query = 'SELECT * FROM alumni WHERE user_id = ?';
     dbConnection.query(query, [userId], (err, result) => {
         if (err) {
-            console.error("Database error:", err);  
+            console.error("Database error:", err);
             return res.status(500).json({ message: 'Database error', error: err });
         }
 
         if (result.length === 0) {
             return res.status(404).json({ message: 'Alumni not found' });
         }
-
-        res.render('AlumniFeed', { AlumniFeed: result[0] });  
+        res.sendFile(path.join(__dirname, '../view/AlumniFeed.html')); 
+        //res.json(result[0])
     });
 };
--- alumni/index.js
+++ alumni/index.js
@@ -12,10 +12,10 @@ const app = express();
 app.use(express.json()); 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); 
-app.use(express.static(path.join(__dirname, "assets"))); 
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "view")); 
+ 
 app.use("/alumni", alumniRoutes);
 
 app.get('/user-login', (req, res) => {
@@ -25,7 +25,7 @@ app.get('/user-login', (req, res) => {
         res.cookie('user_id', userId, { httpOnly: true, secure: false, maxAge: 3600000 }); 
         return res.redirect('/alumni');  
     } else {
-        res.status(400).send('User ID is missing!');
+        res.status(400).send('Invalid access!');
     }
 });
 
