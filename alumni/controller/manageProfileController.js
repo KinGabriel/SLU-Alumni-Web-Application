cac949bd2e3a94b9da7836513fdb67baaf67ba88
KinGabriel <joaquingabriel803@gmail.com>
2024-12-11 19:03:28 +0800

    created the backend for the update password

--- alumni/controller/manageProfileController.js
+++ alumni/controller/manageProfileController.js
@@ -1,4 +1,5 @@
 import dbConnection from '../../database/connection.js';
+import bcrypt from 'bcryptjs';
 
 export const getProfileInfo = async (req, res) => {
     const userId = req.userId;
@@ -49,3 +50,36 @@ export const getProfileInfo = async (req, res) => {
 export const updateProfile = async (req, res) => {
 
 }
+
+export const updatePassword = async (req, res) => {
+    const userId = req.userId; 
+    const { currentPassword, newPassword } = req.body;
+    const query = `SELECT pword FROM user WHERE user_id = ?;`;
+    dbConnection.query(query, [userId], async (error, results) => {
+        if (error) {
+            console.error('Database error:', error);
+            return res.status(500).json({ error: 'Failed to fetch profile information.' });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+
+        const user = results[0];
+        const isMatch = await bcrypt.compare(currentPassword, user.pword);
+        if (!isMatch) {
+            return res.status(400).json({ error: 'Current password is incorrect.' });
+        }
+        const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+
+        const updateQuery = `UPDATE user SET pword = ? WHERE user_id = ?;`;
+        dbConnection.query(updateQuery, [hashedNewPassword, userId], (updateError, updateResults) => {
+            if (updateError) {
+                console.error('Error updating password:', updateError);
+                return res.status(500).json({ error: 'Error updating password.' });
+            }
+
+            return res.status(200).json({ message: 'Password updated successfully.' });
+        });
+    });
+};
--- alumni/routes/manangeProfileRoute.js
+++ alumni/routes/manangeProfileRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getProfileInfo,updateProfile} from '../controller/manageProfileController.js';
+import {getProfileInfo,updateProfile,updatePassword} from '../controller/manageProfileController.js';
 import authenticateMiddleware from '../middleware/authenticateMiddleware.js';
 
 
@@ -7,8 +7,8 @@ const router = express.Router();
 
 router.use(authenticateMiddleware);
 
-router.get('/get-info', getProfileInfo)
-router.get('/update-details',updateProfile )
-
+router.get('/get-info', getProfileInfo);
+router.get('/update-details',updateProfile );
+router.post('change-password', updatePassword);
 
 export default router;
