524b50204a451890dded55dfd7a510cac32844d7
Carlos Vergara <2234487@slu.edu.ph>
2024-12-13 16:36:22 +0800

    dashboard recently added

--- admin/view/userRequest.php
+++ admin/view/userRequest.php
@@ -101,29 +101,7 @@ require("../controller/HandleSession.php");
             <tbody id="applicantTableBody">
                
             </tbody>
-        </table>
-
-
-         <!-- Pagination -->
-         <div class="pagination">
-                <ul class="pagination">
-                    <li class="page-item disabled">
-                        <span class="page-link">&laquo;</span>
-                    </li>
-                    <li class="page-item active">
-                        <span class="page-link">1</span>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="#">2</a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="#">3</a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="#">Next &raquo;</a>
-                    </li>
-                </ul>
-            </div>
+        </table>    
         </div>
 
     </div>
